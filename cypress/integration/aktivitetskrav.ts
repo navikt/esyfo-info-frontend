ea687151acd5cf4ad7259373db8a2a435476975a
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-05-11 14:26:49 +0200

    Forbedret test

--- cypress/integration/aktivitetskrav.ts
+++ cypress/integration/aktivitetskrav.ts
@@ -11,6 +11,39 @@ describe('Tester aktivitetskrav', () => {
                 'contain',
                 'Det er viktig at du har god dialog med arbeidsgiveren din om hva som skal til for at du kan være noe i arbeid. Hvis dere ikke har hatt dialog, ta kontakt og be om en samtale.'
             )
+            .should(
+                'not.contain',
+                'Er du selvstendig næringsdrivende eller frilanser, er det kanskje noen av arbeidsoppgavene dine du fortsatt kan utføre? Er du arbeidsledig og sykmeldt, er det viktig å planlegge hvilke aktiviteter eller tiltak du har behov for slik at du kan komme i jobb igjen.'
+            )
+
+        cy.contains('Jeg har ikke arbeidsgiver').click()
+
+        cy.get('.artikkel')
+
+            .should(
+                'not.contain',
+                'Det er viktig at du har god dialog med arbeidsgiveren din om hva som skal til for at du kan være noe i arbeid. Hvis dere ikke har hatt dialog, ta kontakt og be om en samtale.'
+            )
+            .should(
+                'contain',
+                'Er du selvstendig næringsdrivende eller frilanser, er det kanskje noen av arbeidsoppgavene dine du fortsatt kan utføre? Er du arbeidsledig og sykmeldt, er det viktig å planlegge hvilke aktiviteter eller tiltak du har behov for slik at du kan komme i jobb igjen.'
+            )
+
+        cy.contains('Jeg har arbeidsgiver').click()
+
+        cy.get('.artikkel')
+            .should(
+                'contain',
+                'Du har snart vært sykmeldt i åtte uker. NAV skal vurdere om du oppfyller aktivitetsplikten og om du fortsatt har rett til sykepenger.'
+            )
+            .should(
+                'contain',
+                'Det er viktig at du har god dialog med arbeidsgiveren din om hva som skal til for at du kan være noe i arbeid. Hvis dere ikke har hatt dialog, ta kontakt og be om en samtale.'
+            )
+            .should(
+                'not.contain',
+                'Er du selvstendig næringsdrivende eller frilanser, er det kanskje noen av arbeidsoppgavene dine du fortsatt kan utføre? Er du arbeidsledig og sykmeldt, er det viktig å planlegge hvilke aktiviteter eller tiltak du har behov for slik at du kan komme i jobb igjen.'
+            )
     })
 })
 
