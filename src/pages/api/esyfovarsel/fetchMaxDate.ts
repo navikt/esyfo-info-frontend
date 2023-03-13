baeee87fc39fe3a0080e391bed603ef7dc88da75
Tanja Krasnomovets <victorandreevi4@gmail.com>
2023-03-13 06:56:19 +0100

    Add response json to handler

--- src/pages/api/esyfovarsel/fetchMaxDate.ts
+++ src/pages/api/esyfovarsel/fetchMaxDate.ts
@@ -1,7 +1,10 @@
-import { get } from 'https'
+import { RequestOptions } from 'https'
+import * as https from 'https'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { Readable } from 'stream'
 
 import { beskyttetApi } from '../../../auth/beskyttetApi'
+import { stream2buffer } from '../../../proxy/stream2buffer'
 import { esyfovarselHost, isMockBackend } from '../../../utils/environment'
 import { getEsyfovarselTokenFromRequest } from '../../../utils/tokenX/getTokenXFromRequest'
 
@@ -14,20 +17,49 @@ const handler = async (
     } else {
         const tokenX = await getEsyfovarselTokenFromRequest(req)
 
-        const response = await getMaxDate(tokenX)
-        res.status(200).json(response)
+        await getMaxDate(tokenX, req, res)
+        res.status(200)
     }
 }
 
 export default beskyttetApi(handler)
 
-export async function getMaxDate(accessToken: string) {
-    return get(`${esyfovarselHost()}/api/v1/sykepenger/maxdate`, {
+export async function getMaxDate(
+    accessToken: string,
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const stream = Readable.from(req)
+    const bodyin = await stream2buffer(stream)
+    const options: RequestOptions = {
+        port: 443,
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Nav-Consumer-Id': 'esyfo-info-frontend',
             port: 443,
             Authorization: `Bearer ${accessToken}`,
         },
-    })
+    }
+
+    const backendRequest = https.request(
+        `${esyfovarselHost()}/api/v1/sykepenger/maxdate`,
+        options,
+        (backendResponse) => {
+            if (backendResponse.statusCode != null) {
+                res.status(backendResponse.statusCode)
+            }
+            for (const headersKey in backendResponse.headers) {
+                res.setHeader(headersKey, backendResponse.headers[headersKey]!)
+            }
+            backendResponse.on('data', (d: any) => {
+                res.write(d)
+            })
+            backendResponse.on('end', (d: any) => {
+                res.end()
+            })
+        }
+    )
+    backendRequest.write(bodyin)
+    backendRequest.end()
 }
