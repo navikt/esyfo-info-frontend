f8cb8cb1f50270be23cca40f583e93981f0859a4
Luan Tran <8613937+luantr@users.noreply.github.com>
2023-08-04 09:22:19 +0200

    add new acr token validation (#40)

--- src/utils/idporten/verifyIdportenToken.ts
+++ src/utils/idporten/verifyIdportenToken.ts
@@ -40,8 +40,11 @@ export async function validateToken(bearerToken: string): Promise<boolean> {
         return false
     }
 
-    if (verified.payload.acr !== 'Level4') {
-        logger.warn('Token does not have acr Level4')
+    if (
+        verified.payload.acr !== 'Level4' &&
+        verified.payload.acr !== 'idporten-loa-high'
+    ) {
+        logger.warn('Token does not have acr Level4 or idporten-loa-high')
         return false
     }
 
