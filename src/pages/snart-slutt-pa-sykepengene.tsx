c539f37b06d1137134965d7b0f5a64cf4aa83d53
AudunSorheim <audun.sorheim@nav.no>
2023-09-01 14:32:44 +0200

    Revert snartslutt content

--- src/pages/snart-slutt-pa-sykepengene.tsx
+++ src/pages/snart-slutt-pa-sykepengene.tsx
@@ -1,9 +1,38 @@
+import { Button, Link } from '@navikt/ds-react'
 import React from 'react'
+import { dittSykefravaerUrl } from '../utils/environment'
+import { ChevronLeftIcon } from '@navikt/aksel-icons'
 import { beskyttetSideUtenProps } from '../auth/beskyttetSide'
-import { SnartSluttPaaSykepengeneContent } from '../components/snart-slutt/SnartSluttPaaSykepengeneContent'
+import { SnartSluttLenkePanel } from '../components/snart-slutt/SnartSluttLenkePanel'
+import { SnartSluttIngress } from '../components/snart-slutt/SnartSluttIngress'
+import { SnartSluttHvaKanDuGjore } from '../components/snart-slutt/SnartSluttHvaKanDuGjore'
+import { SnartSluttOnskerDuMerVeiledning } from '../components/snart-slutt/SnartSluttOnskerDuMerVeiledning'
+import { SnartSluttSporsmalOmAndreTing } from '../components/snart-slutt/SnartSluttSporsmalOmAndreTing'
+import { Page } from '../components/page/Page'
 
 const SnartSluttPaSykepengene = () => {
-    return <SnartSluttPaaSykepengeneContent />
+    return (
+        <Page headerText="Snart slutt på sykepengene - hva skjer nå?">
+            <SnartSluttIngress />
+
+            <SnartSluttHvaKanDuGjore />
+
+            <SnartSluttOnskerDuMerVeiledning />
+
+            <SnartSluttLenkePanel />
+
+            <SnartSluttSporsmalOmAndreTing />
+
+            <Link href={dittSykefravaerUrl()}>
+                <Button
+                    variant="tertiary"
+                    icon={<ChevronLeftIcon aria-hidden />}
+                >
+                    Ditt sykefravaer
+                </Button>
+            </Link>
+        </Page>
+    )
 }
 
 export const getServerSideProps = beskyttetSideUtenProps
